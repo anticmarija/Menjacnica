922ee2bc7bc6585d5904980978a2090a67844cbe
majaantic3113 <majaantic994@gmail.com>
2015-03-25 10:58:32 +0100

    Napravljena klasa RadSaValutama koja implementira interfejs MenjacnicaInterfejs.

---
+++ src/menjacnica/interfejs/RadSaValutama.java
@@ -0,0 +1,22 @@
+package menjacnica.interfejs;
+
+import java.util.GregorianCalendar;
+
+public class RadSaValutama implements MenjacnicaInterfejs {
+
+	public void dodajKursNaDan(String valuta, double kurs, GregorianCalendar dan) {
+		
+
+	}
+
+	public void obrisiKursZaDan(String valuta, GregorianCalendar dan) {
+		
+
+	}
+
+	public double pronadjiKursZaDan(String valuta, GregorianCalendar dan) {
+		
+		return 0;
+	}
+
+}

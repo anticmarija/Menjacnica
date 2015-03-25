56efd078c8670d7575374fb0c7a908448318a7e8
majaantic3113 <majaantic994@gmail.com>
2015-03-25 11:19:09 +0100

    Implemnetirane metode interfejsa u klasi RadSaValutama.

--- src/menjacnica/interfejs/RadSaValutama.java
+++ src/menjacnica/interfejs/RadSaValutama.java
@@ -1,22 +1,45 @@
 package menjacnica.interfejs;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
+
+import menjacnica.Valuta;
 
 public class RadSaValutama implements MenjacnicaInterfejs {
+	
+	private LinkedList <Valuta> valute = new LinkedList<Valuta>();
 
 	public void dodajKursNaDan(String valuta, double kurs, GregorianCalendar dan) {
 		
+		Valuta v = new Valuta();
+		v.setDatum(dan);
+		v.setSrednjiKurs(kurs);
+		v.setNaziv(valuta);
+		
+		valute.add(v);
 
 	}
 
 	public void obrisiKursZaDan(String valuta, GregorianCalendar dan) {
 		
+		for (int i = 0; i < valute.size(); i++) {
+			if (!valute.get(i).getDatum().before(dan) && !valute.get(i).getDatum().after(dan)) {
+				
+				valute.remove(valute.get(i));
+			}
+		}
 
 	}
 
 	public double pronadjiKursZaDan(String valuta, GregorianCalendar dan) {
 		
-		return 0;
+		double vrednost = 0;
+		for (int i=0; i < valute.size(); i++) {
+			if (valute.get(i).getNaziv().equals(valuta) && 
+					!valute.get(i).getDatum().before(dan) && !valute.get(i).getDatum().after(dan))
+				vrednost = valute.get(i).getSrednjiKurs();
+		}
+		return vrednost;
 	}
 
 }
